74959a66474b7456cd7761d58cf7f0cf7f55b48d
Yen <jason2888940@gmail.com>
2016-12-15 15:15:55 +0800

    formatting complete

--- Server/Source/SE_MySQL.cpp
+++ Server/Source/SE_MySQL.cpp
@@ -24,5 +24,19 @@ SE_MySQL::format(){
 	query("DROP DATABASE IF EXISTS se_database;");
 	query("CREATE DATABASE se_database;");
 	query("USE se_database;");
-	query("CREATE TABLE employee(ID char(255) NOT NULL UNIQUE,Emp_Position int NOT NULL,Emp_Name text,Gender bool,branch text);");
+	query(string("CREATE TABLE employee(")+
+				"ID char(255) NOT NULL UNIQUE,"+
+				"Passwd char(255) NOT NULL,"
+				"Emp_Position int NOT NULL,"+
+				"Default_time int NOT NULL,"+
+				"Emp_Name text,"+
+				"Gender bool,"+
+				"branch text);");
+	query("INSERT INTO employee VALUES('A001','00000000',1,3,NULL,NULL,NULL);");
+	query("INSERT INTO employee VALUES('A002','00000000',2,1,NULL,NULL,NULL);");
+	query("INSERT INTO employee VALUES('A003','00000000',2,2,NULL,NULL,NULL);");
+	for(int i=4;i<14;i++)
+		query("INSERT INTO employee VALUES('A0"+string(1,i/10+48)+string(1,i%10+48)+"','00000000',3,1,NULL,NULL,NULL);");
+	for(int i=14;i<24;i++)
+		query("INSERT INTO employee VALUES('A0"+string(1,i/10+48)+string(1,i%10+48)+"','00000000',3,2,NULL,NULL,NULL);");
 }
--- Server/Source/format.sql
+++ Server/Source/format.sql
@@ -3,8 +3,12 @@ CREATE DATABASE se_database;
 USE se_database;
 CREATE TABLE employee(
 ID char(255) NOT NULL UNIQUE,
+Passwd char(255) NOT NULL,
 Emp_Position int NOT NULL,
+Default_time int NOT NULL,
 Emp_Name text,
 Gender bool,
 branch text
 );
+INSERT INTO employee
+VALUES('A001','00000000',1,3,NULL,NULL,NULL);
